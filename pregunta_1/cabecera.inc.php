8e110c52de9e8182c8cb93dfc118591c827969cd
1234RoDrIgO <111301830+1234RoDrIgO@users.noreply.github.com>
2024-05-06 02:31:31 -0400

    Add files via upload

---
+++ pregunta_1/cabecera.inc.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PREGUNTA 1</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<ul class="menu-horizontal">
+				<li>
+					<a href="#Caja-de-ahorro">Caja de ahorro</a>
+					<ul class="menu-vertical">
+						<li><a href="#Caja-de-ahorro Pro-EDUCA">Caja de ahorro Pro-EDUCA</a></li>
+						<li><a href="#Caja-de-ahorro Pro-RENTABLE">Caja de ahorro Pro-RENTABLE</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="#Cuenta-corriente">Cuenta corriente</a>
+				</li>
+				<li><a href="#DPF">DPF</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+
+		<div class="item" id="Caja-de-ahorro">
---
+++ pregunta_1/index.php
@@ -0,0 +1,418 @@
+<?php include "cabecera.inc.php";?>
+            <h1>Caja de ahorro</h1>
+			<br>
+			<p>Es un producto de ahorro, que permite al cliente realizar depósitos y retiros de forma ilimitada, los
+				fondos depositados son de disponibilidad inmediata y generan intereses que son capitalizados
+				mensualmente</p>
+			<br>
+			<h2>Información</h2>
+			<br>
+			<h4>Condiciones De La Caja De Ahorro</h4>
+			<center>
+				<img src="imagenes/caja-de-ahorro-1.png" alt="20">
+				<img src="imagenes/caja-de-ahorro-2.png" alt="20">
+			</center>
+
+			<br>
+			<h2>Requisitos</h2>
+			<br>
+
+			<table>
+				<tr>
+					<th>Categoría</th>
+					<th>Documentos requeridos</th>
+				</tr>
+				<tr>
+					<td>Personas Naturales</td>
+					<td>
+						<ol>
+							<li>Boliviano: Cédula de Identidad vigente.</li>
+							<li>Extranjero: Cédula de Identidad Extranjero o Documento especial de identificación.</li>
+						</ol>
+					</td>
+				</tr>
+				<tr>
+					<td>Personas Jurídicas</td>
+					<td>
+						<ol>
+							<li>
+								Documentación legal según tipo de empresa.
+							</li>
+						</ol>
+					</td>
+				</tr>
+			</table>
+			<h2>Caracteristicas</h2>
+			<br>
+			<div class="caja1">
+				<ul>
+					<li>Disponible para Personas Naturales y Jurídicas.</li>
+					<br>
+					<li>Sin costo de mantenimiento de cuenta o tarjeta de débito.</li>
+					<br>
+					<li>Disponible en bolivianos y dólares.</li>
+					<br>
+					<li>Puedes abrir tu cuenta de manera individual o colectiva con orden de manejo conjunto o
+						indistinto.
+					</li>
+					<br>
+					<li><b>Para Cuentas en ME:</b> El monto mínimo de apertura debe ser a través de depósito en efectivo
+						en
+						dólares mediante cajas.</li>
+				</ul>
+			</div>
+
+
+			<br>
+			<h2>Ventajas</h2>
+			<br>
+			<div class="caja1">
+				<h4>Beneficios</h4>
+				<br>
+				<ul>
+					<li>Tarjeta de Débito, con la que puedes hacer compras por internet y POS.</li>
+					<br>
+					<li>Acceso a la red más grande del País con más de 190 agencias y más de 440 cajeros automáticos.
+					</li>
+					<br>
+					<li>Manejo de la cuenta y acceso a información de los movimientos a nivel nacional.</li>
+					<br>
+					<li>Contamos con puntos de atención desde las 7 a.m.</li>
+					<br>
+					<li>Acceso a los servicios de Banca Electrónica UNInet plus y UNImóvil plus las 24 horas del día.
+					</li>
+					<br>
+					<li>Consulta tu saldo y movimientos fácil y sin costo través de nuestra Banca Electrónica UNInet
+						plus, UNImóvil plus y Cajeros Automático.</li>
+				</ul>
+			</div>
+		</div>
+
+
+		<div class="item" id="Caja-de-ahorro Pro-EDUCA">
+			<h1>Caja de ahorro Pro-EDUCA</h1>
+			<br>
+			<p>Cuenta de caja de ahorro en bolivianos o dólares, de fácil acceso, que te permite ahorrar y administrar
+				tu dinero de forma segura.</p>
+			<br>
+			<div class="caja1">
+				<h4>Ventajas:</h4>
+				<ul>
+					<li>
+						<b>Libre disponibilidad.</b> La cuenta de caja de ahorro, te permite ahorrar y ganar intereses
+						con
+						el
+						beneficio de retirar y depositar tu dinero cuando así lo requieras, sin montos mínimos de
+						depósitos
+						y con número ilimitado de transacciones. Podrás administrar tu cuenta de caja de ahorro con
+						Tarjeta
+						Inteligente o con Comprobante.
+					</li>
+					<br>
+					<li>
+						<b>Seguridad.</b> Gracias al moderno sistema de autentificación de la huella digital, sin claves
+						ni
+						pines,
+						ni papeleos.
+						Tecnología. La caja de ahorro con tarjeta inteligente, te permite realizar transacciones en
+						cualquiera de nuestros puntos de atención y a través de nuestros Cajeros Automáticos
+						Inteligentes
+						(CAI), las 24 horas del día.
+					</li>
+					<br>
+					<li>
+						<b>Sin costos.</b> La cuenta de caja de ahorro NO tiene costo de mantenimiento de cuenta, ni
+						costo
+						de mantenimiento de Tarjeta Inteligente.
+					</li>
+					<br>
+					<li>
+						<b>Cobertura.</b> Banco Prodem S.A. cuenta con más de 330 puntos de atención distribuidos en
+						todo el
+						país, los
+						cuales comprenden, agencias, oficinas externas, puntos de atención corresponsal y cajeros
+						automáticos
+						inteligentes en todo el país.
+					</li>
+					<br>
+					<li>
+						<b>Canales de Acceso.</b> Podrás acceder a tu caja de ahorro desde los siguientes canales de
+						atención:
+						<div class="caja1-1">
+							<ul class="ul1">
+								<li>Ventanilla de atención</li>
+								<li>Cajeros Automáticos Inteligentes (CAI) de la red Prodem</li>
+								<li>Banca por internet (Prodemnet) y banca móvil (Prodemmóvil)
+								</li>
+							</ul>
+						</div>
+					</li>
+					<br>
+					<li>
+						<b>Requisitos de Apertura</b>
+						<div class="caja1-1">
+							<ul class="ul1">
+								<li>Cédula de Identidad vigente (original y fotocopia).</li>
+								<li>Fotocopia de NIT, si corresponde.</li>
+								<li>Monto mínimo de apertura es de bolivianos 50.- o dólares 8.-</li>
+								<li>Costo de la Tarjeta Inteligente de acuerdo a tarifario vigente.</li>
+
+							</ul>
+						</div>
+					</li>
+				</ul>
+			</div>
+
+			<br>
+			<p>
+				Para mayor información consulta a nuestra línea gratuita 800-10-9797
+			</p>
+
+		</div>
+
+
+		<div class="item" id="Caja-de-ahorro Pro-RENTABLE">
+			<h1>Caja de ahorro Pro-RENTABLE</h1>
+			<br>
+			<p>
+				Cuenta de caja de ahorro en bolivianos, que te ofrece una de las más alta tasas de interés del mercado,
+				premiando la permanencia de tus ahorros, brindándote MAYORES GANANCIAS PARA UN MEJOR FUTURO.
+			</p>
+			<br>
+			<div class="caja1">
+				<h4>Ventajas: </h4>
+				<br>
+				<ul>
+					<li><b>Rentabilidad.</b> Te ofrecemos una de las tasas de interés más altas del mercado (ver
+						tarifario
+						vigente).</li>
+					<br>
+					<li><b>Seguridad.</b>Gracias al moderno sistema de autentificación de la huella digital, sin claves
+						ni
+						pines, ni papeleos.</li>
+					<br>
+					<li><b>Tecnología.</b> La caja de ahorro con tarjeta inteligente, te permite realizar transacciones
+						en
+						cualquiera de nuestros puntos de atención y a través de nuestros Cajeros Automáticos
+						Inteligentes
+						(CAI), las 24 horas del día.</li>
+					<br>
+					<li><b>Sin costos.</b> La cuenta de caja de ahorro NO tiene costo de mantenimiento de cuenta, ni
+						costo
+						de mantenimiento de Tarjeta Inteligente.</li>
+					<br>
+					<li><b>Cobertura.</b>anco Prodem S.A. cuenta con más de 330 puntos de atención distribuidos en todo
+						el
+						país, los cuales comprenden, agencias, oficinas externas, puntos de atención corresponsal y
+						cajeros
+						automáticos inteligentes en todo el país.</li>
+					<br>
+					<li><b>Canales de Acceso.</b>Podrás acceder a tu caja de ahorro desde los siguientes canales de
+						atención:
+						<div class="caja1-1">
+							<ul class="ul1">
+								<li>Ventanilla de atención</li>
+								<li>Cajeros Automáticos Inteligentes (CAI) de la red Prodem</li>
+								<li>Banca por Internet (prodemnet) y banca mòvil (Prodemmòvil) </li>
+							</ul>
+						</div>
+					</li>
+					<br>
+					<li><b>Requisitos de Apertura</b>
+						<div class="caja1-1">
+							<ul class="ul1">
+								<li>Cédula de Identidad vigente (original y fotocopia)</li>
+								<li>Fotocopia de NIT, si corresponde.</li>
+								<li>Monto mínimo de apertura bolivianos 7.000.-</li>
+								<li>Saldo mínimo en la cuenta Bs 7.000.-</li>
+								<li>Condiciones: Hasta 3 retiros al mes</li>
+								<li>Costo de Tarjeta inteligente de acuerdo a tarifario vigente</li>
+							</ul>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<br>
+			<p>
+				Para mayor información consulta a nuestra línea gratuita 800-10-9797
+			</p>
+		</div>
+
+		<div class="item" id="Cuenta-corriente">
+			<h1>Cuenta corriente</h1>
+			<br>
+			<p>Es una cuenta por la cual una persona natural o jurídica denominada cuentacorrentista entrega, por sí o
+				por medio de un tercero, cantidades sucesivas de dinero, pagaderos a su presentación, cuando se realice
+				la solicitud por medio del giro de cheques.</p>
+			<br>
+			<h2>Información</h2>
+			<br>
+			<h4>Condiciones De La Caja De Ahorro</h4>
+			<center>
+				<img src="imagenes/cuenta-corriente-1.png" alt="">
+				<img src="imagenes/cuenta-corriente-2.png" alt="">
+			</center>
+			<br>
+			<h2>Ventajas</h2>
+			<br>
+			<div class="caja1">
+			<h4>Beneficios</h4>
+			<br>
+				<ul>
+					<li>Tarjeta de Débito, con la que puedes hacer compras por internet y POS.</li>
+					<br>
+					<li>Acceso a información de sus movimientos desde cualquier punto de atención en el país.</li>
+					<br>
+					<li>Contamos con puntos de atención desde las 7 a.m.</li>
+					<br>
+					<li>Acceso a los servicios de Banca Electrónica UNInet plus y UNImóvil plus.</li>
+					<br>
+					<li>Estado de cuenta que permite llevar el control de todos los movimientos que efectúa.</li>
+				</ul>
+			</div>
+			<br>
+			<h2>Caracteristicas</h2>
+			<br>
+			<div class="caja1">
+				<ul>
+					<li>Disponible para Personas Naturales y Jurídicas.</li>
+					<br>
+					<li>La cuenta dispone de un talonario de cheques.</li>
+					<br>
+					<li>Sin costo de mantenimiento de cuenta o tarjeta de débito.</li>
+					<br>
+					<li>Disponible en bolivianos y dólares.</li>
+					<br>
+					<li>Puedes abrir tu cuenta de manera individual o colectiva con orden de manejo conjunto o
+						indistinto.</li>
+					<br>
+					<li><b>Para Cuentas en ME:</b> El monto mínimo de apertura debe ser a través de depósito en efectivo
+						en dólares mediante cajas.</li>
+				</ul>
+			</div>
+			<h2>Requisitos</h2>
+			<br>
+
+			<table>
+				<tr>
+					<th>Categoría</th>
+					<th>Documentos requeridos</th>
+				</tr>
+				<tr>
+					<td>Personas Naturales</td>
+					<td>
+						<ol>
+							<li>Boliviano: Cédula de Identidad vigente.</li>
+							<li>Extranjero: Cédula de Identidad Extranjero o Documento especial de identificación.</li>
+						</ol>
+					</td>
+				</tr>
+				<tr>
+					<td>Personas Jurídicas</td>
+					<td>
+						<ol>
+							<li>
+								Documentación legal según tipo de empresa.
+							</li>
+						</ol>
+					</td>
+				</tr>
+			</table>
+		</div>
+
+		<div class="item" id="DPF">
+			<h1>DPF</h1>
+			<br>
+			<p>Es instrumento de inversión inmediata, que luego de constituirse permanece inmóvil por el plazo
+				previamente pactado entre el banco y el cliente. Nuestros DPF´s te ofrecen excelentes rendimientos en
+				base al plazo y monto invertido con pago de intereses periódicos o al vencimiento.</p>
+			<br>
+
+			<h2>Requisitos</h2>
+			<br>
+			<table>
+				<tr>
+					<th>Categoría</th>
+					<th>Documentos requeridos</th>
+				</tr>
+				<tr>
+					<td>Personas Naturales</td>
+					<td>
+						<ol>
+							<li>Boliviano: Cédula de Identidad vigente.</li>
+							<li>Extranjero: Cédula de Identidad Extranjero o Documento especial de identificación.</li>
+						</ol>
+					</td>
+				</tr>
+				<tr>
+					<td>Personas Jurídicas</td>
+					<td>
+						<ol>
+							<li>
+								Documentación legal según tipo de empresa.
+							</li>
+						</ol>
+					</td>
+				</tr>
+			</table>
+
+
+			<h2>Caracteristicas</h2>
+			<br>
+			<div class="caja1-1">
+				<ul>
+					<li>Disponible para Personas Naturales y Jurídicas.</li>
+					<br>
+					<li>Generan intereses en función al plazo del depósito, que pueden ser pagados de manera periódica o
+						al
+						vencimiento.</li>
+					<br>
+					<li>Disponible en bolivianos y dólares.</li>
+					<br>
+					<li>Cancelación anticipada para DPF con un plazo menor o igual a 360 días.</li>
+					<br>
+					<li>Renovación automática de tu DPF después del vencimiento.</li>
+					<br>
+					<li>Monto mínimo de constitución de DPF en M/N: Bs. 7,000</li>
+					<br>
+					<li>Monto mínimo de constitución de DPF en M/E para Persona Natural: USD.- 10,000 con deposito en
+						efectivo en dólares mediante cajas</li>
+					<br>
+					<li>Monto mínimo de constitución de DPF en M/E para Persona Juridica: USD.-20,000 con deposito en
+						efectivo en dólares mediante cajas</li>
+				</ul>
+			</div>
+			<br>
+			<h2>Ventajas</h2>
+			<br>
+			<table>
+				<tr>
+					<th>Utilidad</th>
+					<th>Beneficios</th>
+				</tr>
+				<tr>
+					<td>
+						<ul>
+							<li>Inversión inmediata a una tasa de interés atractiva.</li>
+							<li>Seguridad y confianza para sus ahorros, respaldados por la solvencia del Banco de los
+								Bolivianos.</li>
+							<li>Pueden servir de garantía para préstamos.</li>
+							<li>Liquidez inmediata al momento del vencimiento.</li>
+						</ul>
+
+					</td>
+					<td>
+						<ul>
+							<li>Inversión inmediata a una tasa de interés atractiva.</li>
+							<li>Seguridad y confianza para sus ahorros, respaldados por la solvencia del Banco de los
+								Bolivianos.</li>
+							<li>Liquidez inmediata al momento del vencimiento.</li>
+							<li>Aplica como garantía de un crédito</li>
+							<li>Los DPF's en moneda nacional a partir de 31 días estan exentos del pago del Impuesto al
+								Valor Agregado (IVA) sobre los intereses ganados.</li>
+							<li>Los DPF's constituidos en moneda extranjera con un plazo igual o mayor a 1100 días estan
+								exentos del pago del Impuesto al Valor Agregado (IVA) sobre los intereses ganados.</li>
+							<li>Los DPF's en moneda extranjera a partir de 361 días al vencimiento estan exentos del
+								ITF.</li>
+
+<?php include "pie.inc.php";?>                                
